f1e7c30f6ba3adf386cbc05c9998233c5f12db3e
Maxameano <49102528+Maxameano@users.noreply.github.com>
2022-03-06 12:40:16 -0400

    Update P1.js

--- P1.js
+++ P1.js
@@ -1,70 +1,76 @@
 let blogNum = 0;
 
 function setup() {
+  
+  //writing items from local storage to text boxes
+  $('#text1').val(localStorage.getItem('edit1'))
+  $('#text2').val(localStorage.getItem('edit2'))
+  $('#text3').val(localStorage.getItem('edit3'))
 
-    //run onload function
-    onload()
-
-    $("#edit1").change(function() {
-        if(this.checked) {
-            $("#edit2").hide()
-            $("#edit3").hide()
-            blogNum = 1;
-            $("#textArea").show()
-        }else{
-            $("#edit2").show()
-            $("#edit3").show()
-            blogNum = 0;
-            $("#textArea").hide()
-        }
-    });
-
-    $("#edit2").change(function() {
-        if(this.checked) {
-            $("#edit1").hide()
-            $("#edit3").hide()
-            blogNum = 2;
-            $("#textArea").show()
-        }else{
-            $("#edit1").show()
-            $("#edit3").show()
-            blogNum = 0;
-            $("#textArea").hide()
-        }
-    });
-
-    $("#edit3").change(function() {
-        if(this.checked) {
-            $("#edit1").hide()
-            $("#edit2").hide()
-            blogNum = 3;
-            $("#textArea").show()
-        }else{
-            $("#edit1").show()
-            $("#edit2").show()
-            blogNum = 0;
-            $("#textArea").hide()
-        }
-    });
+  //hides the text area on load
+  $("#textArea").hide()
     
-    //Listener for submit button
-    $('#submit').click(()=>{
-        
-        //check if storage is available
-        if(typeof Storage !=='undefined'){
-        
-            //get the word from user
-            var input = $('#words').val();
+  //listeners for the three edit toggles, which will hide each other and reveal the keyboard/textbox
+  $("#edit1").change(function() {
+    if(this.checked) {
+      $("#edit2").hide()
+      $("#edit3").hide()
+      $("#textArea").show()
+      blogNum = 1;
+    }else{
+      $("#edit2").show()
+      $("#edit3").show()
+      $("#textArea").hide()
+      blogNum = 0;
+    }
+  });
 
-            //emptying the text box
-            $('#words').val('')
+  $("#edit2").change(function() {
+    if(this.checked) {
+      $("#edit1").hide()
+      $("#edit3").hide()
+      $("#textArea").show()
+      blogNum = 2;
+    }else{
+      $("#edit1").show()
+      $("#edit3").show()
+      $("#textArea").hide()
+      blogNum = 0;
+    }
+  });
+
+  $("#edit3").change(function() {
+    if(this.checked) {
+      $("#edit1").hide()
+      $("#edit2").hide()
+      $("#textArea").show()
+      blogNum = 3;
+    }else{
+      $("#edit1").show()
+      $("#edit2").show()
+      $("#textArea").hide()
+      blogNum = 0;
+    }
+  });
+
+  //Listener for submit button
+  $('#submit').click(()=>{
         
-            //Store stringified object to localStorage
-            window.localStorage.setItem('edit'+blogNum,input)
+    //check if storage is available
+    if(typeof Storage !=='undefined'){
+    
+      //get the word from user
+      var input = $('#words').val();
+
+      //emptying the text box
+      $('#words').val('')
+        
+      //Store stringified object to localStorage
+      window.localStorage.setItem('edit'+blogNum,input)
 
     //else if there is no local storage
     }else{
-        console.log('no local storage available')
+      console.log('no local storage available')
     }
     
     //reseting buttons and text area
@@ -72,42 +78,55 @@ function setup() {
 
     //Write input to edit box
     $('#text'+blogNum).val(input)
-    
     })
     
     //Listener for cancel button
     $('#cancel').click(reset)
 }
 
+function addChar(selection) {
+  // Get the value from the id'ed field
+  var currChars = $("#words").val();
+
+  if (selection === "bksp") {
+    
+    // Set the id'ed field to a shortened string
+    $("#words").val(currChars.substring(0, currChars.length - 1));
+  } else {
+
+    // Set the id'ed field to the longer string
+    $("#words").val(currChars.concat(selection));
+  }
+}
+
+//function to add a linebreak when enter is pressed
+function enter() {
+  var content = $("#words").val();
+  console.log(content);
+  $("#words").val(content+"\n");
+}
+
+//stub function to later handle the shift key
+function shift() {
+  console.log("shift");
+}
+
 //function to hide text area and reset buttons
 function reset() {
     
-    //show buttons
-    $("#edit1").show()
-    $("#edit2").show()
-    $("#edit3").show()
+  //show buttons
+  $("#edit1").show()
+  $("#edit2").show()
+  $("#edit3").show()
         
-    //uncheck buttons
-    $('#edit1').prop('checked',false)
-    $('#edit2').prop('checked',false)
-    $('#edit3').prop('checked',false)
+  //uncheck buttons
+  $('#edit1').prop('checked',false)
+  $('#edit2').prop('checked',false)
+  $('#edit3').prop('checked',false)
         
-    //hide text box and keyboard
-    $('#textArea').hide()
+  //hide text box and keyboard
+  $('#textArea').hide()
 
-    //blanking the text area
-    $('#words').val('')
-}
-
-//function ran onload
-function onload() {
-
-    //writing items from local storage to text boxes
-    $('#text1').val(localStorage.getItem('edit1'))
-    $('#text2').val(localStorage.getItem('edit2'))
-    $('#text3').val(localStorage.getItem('edit3'))
-    
-    
-    //hide text area
-    $('#textArea').hide()
+  //blanking the text area
+  $('#words').val('')
 }
